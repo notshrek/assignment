5ca9ece796f36ae10e92a16d790a3060948e60f7
notshrek <notshrek>
2024-07-23 21:07:38 +0330

    feat(index.ts): update base url

--- index.ts
+++ index.ts
@@ -4,7 +4,7 @@ import router from "./routes";
 const app = express();
 const port = 3000;
 
-app.use("/", router);
+app.use("/api/v1", router);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
