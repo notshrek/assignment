5d11cfc8d693cca0eb8004ae237f660bdc3bad4b
notshrek <notshrek>
2024-07-23 23:54:34 +0330

    feat(routes.ts): add basic /users post route with spec

--- routes.ts
+++ routes.ts
@@ -61,4 +61,59 @@ router.get("/users", (req, res) => {
   res.send("users");
 });
 
+/**
+ * @openapi
+ * /api/v1/users:
+ *   post:
+ *     summary: Add a new user
+ *     description: Creates a new user with the username passed in the request body. The given username must be unique.
+ *     security:
+ *        - BearerAuth: []
+ *     tags:
+ *        - Users
+ *     requestBody:
+ *       description: Must be a json object containing the username.
+ *       required: true
+ *       content:
+ *          application/json:
+ *             schema:
+ *                type: object
+ *                properties:
+ *                   username:
+ *                      type: string
+ *     responses:
+ *       201:
+ *         description: An object containing information about the newly created user.
+ *         content:
+ *            application/json:
+ *               schema:
+ *                  type: object
+ *                  properties:
+ *                     result:
+ *                        type: object
+ *                        properties:
+ *                           id:
+ *                              type: uuid
+ *                           username:
+ *                              type: string
+ *                           joined_at:
+ *                              type: timestamp
+ *               example:
+ *                  result:
+ *                     id: '9625d82e-3ad7-4633-95f5-49e4886ca3ef'
+ *                     username: 'John Doe'
+ *                     joined_at: '1721762939542'
+ *       400:
+ *         description: Malformed request.
+ *       401:
+ *         description: Unauthorized.
+ *       409:
+ *         description: Username already exists.
+ *       5XX:
+ *         description: Server-side error.
+ */
+router.post("/users", (req, res) => {
+  res.send("users");
+});
+
 export default router;
